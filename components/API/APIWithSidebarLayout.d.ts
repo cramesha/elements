8126565f289c8d29f744ea64c0f4ebc5c6d1535d
abdut007 <113962702+abdut007@users.noreply.github.com>
2022-12-09 03:55:22 +0530

    First commit

---
+++ components/API/APIWithSidebarLayout.d.ts
@@ -0,0 +1,17 @@
+import { ExportButtonProps } from '@stoplight/elements-core';
+import * as React from 'react';
+import { ServiceNode } from '../../utils/oas/types';
+declare type SidebarLayoutProps = {
+    serviceNode: ServiceNode;
+    logo?: string;
+    logoLink?: string;
+    hideTryIt?: boolean;
+    hideSchemas?: boolean;
+    hideInternal?: boolean;
+    hideExport?: boolean;
+    exportProps?: ExportButtonProps;
+    tryItCredentialsPolicy?: 'omit' | 'include' | 'same-origin';
+    tryItCorsProxy?: string;
+};
+export declare const APIWithSidebarLayout: React.FC<SidebarLayoutProps>;
+export {};
